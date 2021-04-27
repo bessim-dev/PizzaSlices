796c168e38c4f049559c938b53e86a5f7242ad66
becem-dev <you@example.com>
2021-04-27 13:13:08 +0100

    index header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,7 +38,7 @@ export default function HomePage() {
 
   return (
     <div className="center">
-      <h1>The Best Pizza Downtown!</h1>
+      <h1>The Best Pizza In Tunisia!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
       <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters} />
